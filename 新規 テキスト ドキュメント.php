d7ded14bcafd007a3be4be7be0ddebb7fdccdf89
21316 <kudahu7@gmail.com>
2024-07-06 09:51:04 +0900

    新規 テキスト ドキュメント.php

--- 新規 テキスト ドキュメント.php
+++ 新規 テキスト ドキュメント.php
@@ -1,17 +1,1 @@
-<?php
-$response = $_POST['recaptcha_response'];
-$secret = '6LewLQgqAAAAAB2h4A986PuSCWbphU2T2DaeeZuc';
 
-// Recaptcha APIへのリクエスト
-$verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
-$responseData = json_decode($verifyResponse);
-
-if($responseData->success) {
-    // Recaptcha成功
-    echo "Recaptcha success.";
-    // その他のフォーム処理をここに追加
-} else {
-    // Recaptcha失敗
-    echo "Recaptcha failed.";
-}
-?>
